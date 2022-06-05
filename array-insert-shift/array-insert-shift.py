1b321e87077b34c81069e7ff13b13715aa939c7c
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-06-05 10:53:20 +0300

    update the code

--- array-insert-shift/array-insert-shift.py
+++ array-insert-shift/array-insert-shift.py
@@ -8,6 +8,7 @@
 #         arr.append(num)
 #     return arr
 
+
 # New function without build in len, insert ,append
 def array_insert_shift(arr, n):
     arr_len = 0
@@ -22,20 +23,3 @@ def array_insert_shift(arr, n):
 
 
 print(array_insert_shift([2, 4, 9, 6, -8], 5))
-
-
-""" 
-Pseudo Code
-
-defined array-insert-shift(array,number) :
-    declare new variable arrLen = 0 
-    for i in array 
-        increment the arrLen to count the length on the array
-    if the arrLen of array %2 != 0 "odd" 
-        divide it by 2 then + 1
-    else arrLen is even 
-        divide it by 2
-    declare new array = arr[start to:arrLen] concat with +[number]+ concat with the rest of the array arr[arrLen:]
-    retun the new array 
-    
-"""
