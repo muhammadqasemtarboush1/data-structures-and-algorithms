0551878deba8927520716bbcb2b4f9cb3957d09a
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-06-13 04:02:01 +0300

    update: add new linkedlist methodes

--- linked_list/README.md
+++ linked_list/README.md
@@ -9,6 +9,9 @@ A linked list is a linear data structure, in which the elements are not stored a
   - insert
   - include
   - to string
+  - append
+  - insert before
+  - insert after
 - Testing
 
 ## Approach & Efficiency
@@ -19,12 +22,25 @@ Time complexity:
  - insert : (1)
  - include : (n)
  - to string : (n)
+ - append : (n)
+ - insert after : (n)
+ - insert before : (n)
 
 Space complexity:
  - insert: (n)
  - include: (1)
  - to string : (1)
+ - append : (1)
+ - insert after : (1)
+ - insert before : (1)
 
+## Whiteboard Process
+* append 
+  * ![append](append.jpg)
+* insert before 
+  * ![insert before](insert%20before.jpg)
+* insert after
+  * ![insert after](insert%20after.jpg)
 ## API
 <!-- Description of each method publicly available to your Linked List -->
 > Insert:
@@ -37,3 +53,18 @@ Space complexity:
 > 
 > To string:
 >> return a collection of all the values that exist in the linked list
+> > 
+> Append:
+>> parameters: value
+> 
+>> functionally: adds a new node with the given value to the end of the list
+> 
+> insert before:
+>> parameters: search value, new value
+> 
+>> functionally: adds a new node with the given new value immediately before the first node that has the value specified
+>  
+> insert after:
+>> parameters: search value, new value
+> 
+>> functionally: adds a new node with the given new value immediately after the first node that has the value specified
--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -7,6 +7,10 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def err_msg(self):
+        return "No change, method exception"
+
     # Adds new node containing 'value' to the first node of the linked list.
     def insert(self, value):
         new_node = Node(value)
@@ -17,12 +21,11 @@ class LinkedList:
         else:
             self.head = new_node
 
-
     def to_string(self):
         elems = ''
         current = self.head
         while current:
-            elems += "{ " +str(current.value) +" } ->"
+            elems += "{ " + str(current.value) + " } ->"
             current = current.next
         elems += " NULL"
         return elems
@@ -34,11 +37,51 @@ class LinkedList:
             current = current.next
         return False
 
+    def append(self, value):
+        '''
+         functionally:adds a new node with the given value to the end of the list
+        :param new value:
+        :return: None
+        '''
+        current = self.head
+        n_node = Node(value)
+        if not current:
+            current = n_node
+        else:
+            while current.next:
+                current = current.next
+            current.next = n_node
 
+    def insert_before(self, s_node, value):
+        current = self.head
+        n_value = Node(value)
+        if not current:
+            return self.err_msg()
+        elif current.value == s_node:
+            self.insert(value)
+        else:
+            while current.next:
+                if current.next.value == s_node:
+                    n_value.next = current.next
+                    current.next = n_value
+                    return
+                current = current.next
+            return self.err_msg()
 
-ll = LinkedList()
-ll.insert(5)
-ll.insert(7)
-ll.insert(12)
-ll.insert(0)
-print(ll.to_string())
+    def insert_after(self, s_node, value):
+        current = self.head
+        n_value = Node(value)
+        if not current:
+            return self.err_msg()
+        elif current.value == s_node:
+            n_value.next = self.head
+            self.head = n_value
+        else:
+            while current:
+                if current.value == s_node:
+                    n_value.next = current.next
+                    current.next = n_value
+                    break
+                current = current.next
+            return self.err_msg()
+
