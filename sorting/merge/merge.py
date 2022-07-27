ae2dd3e6d41254fc3cee06df828d863b769b60dd
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-07-27 16:06:37 +0300

    add: init merge sort files& code is done

---
+++ sorting/merge/merge.py
@@ -0,0 +1,77 @@
+'''
+ALGORITHM Mergesort(arr)
+    DECLARE n <-- arr.length
+
+    if n > 1
+      DECLARE mid <-- n/2
+      DECLARE left <-- arr[0...mid]
+      DECLARE right <-- arr[mid...n]
+      // sort the left side
+      Mergesort(left)
+      // sort the right side
+      Mergesort(right)
+      // merge the sorted left and right sides together
+      Merge(left, right, arr)
+
+ALGORITHM Merge(left, right, arr)
+    DECLARE i <-- 0
+    DECLARE j <-- 0
+    DECLARE k <-- 0
+
+    while i < left.length && j < right.length
+        if left[i] <= right[j]
+            arr[k] <-- left[i]
+            i <-- i + 1
+        else
+            arr[k] <-- right[j]
+            j <-- j + 1
+
+        k <-- k + 1
+
+    if i = left.length
+       set remaining entries in arr to remaining values in right
+    else
+       set remaining entries in arr to remaining values in left
+'''
+
+
+def merge(left, right, arr):
+    i = 0
+    j = 0
+    k = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+    return arr
+
+
+def merge_sort(arr):
+    n = len(arr)
+    if n > 1:
+        mid = n // 2
+        left = arr[:mid]
+        right = arr[mid:n]
+        merge_sort(left)
+        merge_sort(right)
+        temp = merge(left, right, arr)
+        return temp
+
+
+if __name__ == "__main__":
+    print(merge_sort([8, 4, 23, 42, 16, 15]))
