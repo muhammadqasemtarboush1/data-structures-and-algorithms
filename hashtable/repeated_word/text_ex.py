f3c31c57b8feba9a4faa96b667d8f69e67ce5da2
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-08-10 22:54:48 +0300

    update: repeated word code & test done

---
+++ hashtable/repeated_word/text_ex.py
@@ -0,0 +1,8 @@
+case_1 = "Once upon a time, there was a brave princess who..."
+expected_1 = "a"
+case_2 = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."
+expected_2 = "it"
+case_3 = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..."
+expected_3 = "summer"
+case_4 = "It was a queer the summer they"
+expected_4 = 'No duplicate founded'
--- tests/test_hashmap_repeated_word.py
+++ tests/test_hashmap_repeated_word.py
@@ -1,1 +1,19 @@
 import pytest
+
+from hashtable.repeated_word.text_ex import *
+from hashtable.repeated_word.repeated_words import *
+
+def test_normal_str():
+    assert repeated_word(case_1) == expected_1
+
+
+def test_with_capitalize_str():
+    assert repeated_word(case_2) == expected_2
+
+
+def test_with_comma_str():
+    assert repeated_word(case_3) == expected_3
+
+
+def test_no_repeate_str():
+    assert repeated_word(case_4) == expected_4
